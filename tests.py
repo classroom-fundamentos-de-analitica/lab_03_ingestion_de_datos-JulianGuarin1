40b677330474001d26f6997c4d729db2160cfa7b
JulianGuarin1 <151401545+JulianGuarin1@users.noreply.github.com>
2024-09-23 18:48:07 -0500

    nuevo_2

--- tests.py
+++ tests.py
@@ -5,191 +5,114 @@ Calificación del laboratorio
 
 import sys
 
-import pandas as pd
-
-import preguntas
+import pregunta
 
 
 def test_01():
-    assert preguntas.pregunta_01() == 40
+    assert pregunta.ingest_data().cluster.to_list() == list(range(1, 14))
 
 
 def test_02():
-    assert preguntas.pregunta_02() == 4
+    assert pregunta.ingest_data().cantidad_de_palabras_clave.to_list() == [
+        105,
+        102,
+        89,
+        60,
+        52,
+        51,
+        42,
+        38,
+        35,
+        27,
+        22,
+        22,
+        17,
+    ]
 
 
 def test_03():
-    assert preguntas.pregunta_03().equals(
-        pd.Series({"A": 8, "B": 7, "C": 5, "D": 6, "E": 14})
-    )
+    assert pregunta.ingest_data().porcentaje_de_palabras_clave.to_list() == [
+        15.9,
+        15.4,
+        13.4,
+        9.1,
+        7.9,
+        7.7,
+        6.3,
+        5.7,
+        5.3,
+        4.1,
+        3.3,
+        3.3,
+        2.6,
+    ]
 
 
 def test_04():
-    assert preguntas.pregunta_04().equals(
-        pd.Series(
-            {
-                "A": 4.625,
-                "B": 5.142857142857143,
-                "C": 5.4,
-                "D": 3.8333333333333335,
-                "E": 4.785714285714286,
-            }
-        )
+
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[0]
+        == "maximum power point tracking, fuzzy-logic based control, photo voltaic (pv), photo-voltaic system, differential evolution algorithm, evolutionary algorithm, double-fed induction generator (dfig), ant colony optimisation, photo voltaic array, firefly algorithm, partial shade"
     )
 
-
-def test_05():
-    assert preguntas.pregunta_05().equals(
-        pd.Series({"A": 9, "B": 9, "C": 9, "D": 7, "E": 9})
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[1]
+        == "support vector machine, long short-term memory, back-propagation neural network, convolution neural network, speed wind prediction, energy consumption, wind power forecasting, extreme learning machine, recurrent-neural-network (rnn), radial basis function (rbf) networks, wind farm"
     )
 
-
-def test_06():
-    assert preguntas.pregunta_06() == ["A", "B", "C", "D", "E", "F", "G"]
-
-
-def test_07():
-    assert preguntas.pregunta_07().equals(
-        pd.Series({"A": 37, "B": 36, "C": 27, "D": 23, "E": 67})
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[2]
+        == "smart grid, wind power, reinforcement learning, energy management, energy efficiency, solar energy, deep reinforcement learning, demand-response (dr), internet of things, energy harvester, q-learning"
     )
 
-
-def test_08():
-    assert preguntas.pregunta_08().columns.tolist() == [
-        "_c0",
-        "_c1",
-        "_c2",
-        "_c3",
-        "suma",
-    ]
-    assert preguntas.pregunta_08().shape == (40, 5)
-    assert preguntas.pregunta_08().suma.tolist() == [
-        1,
-        3,
-        7,
-        6,
-        10,
-        12,
-        15,
-        8,
-        10,
-        12,
-        17,
-        16,
-        15,
-        21,
-        23,
-        16,
-        19,
-        22,
-        26,
-        28,
-        27,
-        24,
-        27,
-        24,
-        28,
-        31,
-        34,
-        32,
-        34,
-        29,
-        31,
-        33,
-        37,
-        37,
-        40,
-        42,
-        44,
-        46,
-        39,
-        44,
-    ]
-
-
-def test_09():
-    assert preguntas.pregunta_09().columns.tolist() == [
-        "_c0",
-        "_c1",
-        "_c2",
-        "_c3",
-        "year",
-    ]
-    assert preguntas.pregunta_09().shape == (40, 5)
-    assert preguntas.pregunta_09().year.head().tolist() == [
-        "1999",
-        "1999",
-        "1998",
-        "1999",
-        "1999",
-    ]
-    assert preguntas.pregunta_09().year.tail().tolist() == [
-        "1999",
-        "1997",
-        "1997",
-        "1999",
-        "1998",
-    ]
-
-
-def test_10():
-    assert preguntas.pregunta_10().equals(
-        pd.DataFrame(
-            {
-                "_c2": [
-                    "1:1:2:3:6:7:8:9",
-                    "1:3:4:5:6:8:9",
-                    "0:5:6:7:9",
-                    "1:2:3:5:5:7",
-                    "1:1:2:3:3:4:5:5:5:6:7:8:8:9",
-                ]
-            },
-            index=pd.Series(["A", "B", "C", "D", "E"], name="_c1"),
-        )
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[3]
+        == "wind turbine, fault diagnosis, biodiesel, failure detection, response-surface methodology, condition monitoring, load forecasting, energy consumption forecast, anomalies detection, optimization-based algorithm, supervisory control and data acquisition"
     )
 
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[4]
+        == "electric vehicle, lithium-ion batteries, state of charge, state of health, hybrid-electric vehicle, energy management strategies, energy management system, remaining useful life, battery management system, transfer learning, hybrid energy storage"
+    )
 
-def test_11():
-    assert preguntas.pregunta_11().columns.tolist() == ["_c0", "_c4"]
-    assert preguntas.pregunta_11().shape == (40, 2)
-    assert preguntas.pregunta_11()._c4.head().tolist() == [
-        "b,f,g",
-        "a,c,f",
-        "a,c,e,f",
-        "a,b",
-        "a,d,f,g",
-    ]
-    assert preguntas.pregunta_11()._c4.tail().tolist() == [
-        "a,f",
-        "a,c",
-        "a,c,e,f",
-        "d,e",
-        "a,d,f",
-    ]
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[5]
+        == "particle swarm optimization, distribute generation, solar irradiance, artificial bee colonies, energy storage systems, bat algorithm, gravitational search algorithm, distributed system, multi-objective swarm optimization (mopso), optimal power-flow (opf), load-frequency control"
+    )
 
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[6]
+        == "multi-objective optimization, energy storage, economic dispatch, non-dominated sorting genetic algorithm, sensitive analysis, hybrid renewable energy source, plug-in electric vehicle, combined-heat and power, multi-objective genetic algorithm, unit-commitment, dc-dc converters"
+    )
 
-def test_12():
-    assert preguntas.pregunta_12().columns.tolist() == ["_c0", "_c5"]
-    assert preguntas.pregunta_12().shape == (40, 2)
-    assert preguntas.pregunta_12()._c5.head().tolist() == [
-        "bbb:0,ddd:9,ggg:8,hhh:2,jjj:3",
-        "aaa:3,ccc:2,ddd:0,hhh:9",
-        "ccc:6,ddd:2,ggg:5,jjj:1",
-        "bbb:1,eee:7,hhh:9,iii:5",
-        "ddd:5,eee:4,iii:6,jjj:3",
-    ]
-    assert preguntas.pregunta_12()._c5.tail().tolist() == [
-        "aaa:0,ddd:3,fff:5",
-        "bbb:4,ccc:0,ddd:5,iii:7,jjj:2",
-        "eee:0,fff:2,hhh:6",
-        "eee:0,fff:9,iii:2",
-        "ggg:3,hhh:8,jjj:5",
-    ]
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[7]
+        == "genetic algorithm, demand-side management, energy-saving, hybrid electric system (hes), wind turbine blade, data-driven modelling, simulated annealing, solar forecasting, geographic information system, renewable energy system, cogeneration"
+    )
 
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[8]
+        == "anfis, global solar irradiance, solar irradiance forecast, genetic programing, islanding detection, expert system, distributed networks, evolutionary computation, wavelet-based neural network, root mean square errors, virtual power plant"
+    )
 
-def test_13():
-    assert preguntas.pregunta_13().equals(
-        pd.Series({"A": 146, "B": 134, "C": 81, "D": 112, "E": 275})
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[9]
+        == "micro grid, multi-agent systems, distributed energy resource, batteries energy storage system, dc micro grid, pitch-control, droop control, hybrid ac/dc microgrid, voltage regulation, superconducting magnetic energy storage, distributed-control"
+    )
+
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[10]
+        == "hydrogen, biochar, biomass, biogas, microbial fuel cell, gasification, biofuel, ethanol, engine performance, pyrolysis, anaerobic digester"
+    )
+
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[11]
+        == "state of charge (soc) estimation, radial basis function, short-term load forecasting, computational fluid dynamics, generalized-regression neural network, monte-carlo simulation, multiple linear regression, power generation, nonlinear auto-regressive exogenous (narx) model neural networks, surrogate model, extreme gradient boosting"
+    )
+
+    assert (
+        pregunta.ingest_data().principales_palabras_clave.to_list()[12]
+        == "pem fuel cell, solid-oxide fuel cell, deep-belief networks, energy optimisation, particles-size distributions, biomass gasification, exergy, battery management, hydrogen production, numeric simulation, system-identification"
     )
 
 
@@ -198,15 +121,6 @@ test = {
     "02": test_02,
     "03": test_03,
     "04": test_04,
-    "05": test_05,
-    "06": test_06,
-    "07": test_07,
-    "08": test_08,
-    "09": test_09,
-    "10": test_10,
-    "11": test_11,
-    "12": test_12,
-    "13": test_13,
 }[sys.argv[1]]
 
 test()
